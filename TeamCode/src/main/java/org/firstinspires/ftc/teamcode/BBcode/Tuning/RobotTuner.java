b81da55b55af120b5187e82727720b40deb16120
Vullarim <swdurda@gmail.com>
2025-10-19 15:30:20 -0500

    Fixed bugs in robot tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/Tuning/RobotTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/Tuning/RobotTuner.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = " Robot Tuner" )
 public class RobotTuner extends OpMode {
@@ -24,6 +25,7 @@ public class RobotTuner extends OpMode {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
+        motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
         leftInserter = hardwareMap.tryGet(CRServo.class, "leftInserter");
         rightInserter = hardwareMap.tryGet(CRServo.class, "rightInserter");
     }
@@ -32,15 +34,17 @@ public class RobotTuner extends OpMode {
     public void loop() {
         if (gamepad1.leftBumperWasPressed()) {
             selectedMotor = Math.max(selectedMotor - 1, 0);
+            isActive = false;
         }
         if (gamepad1.rightBumperWasPressed()) {
             selectedMotor = Math.min(selectedMotor + 1, 2);
+            isActive = false;
         }
         if (gamepad1.dpadUpWasPressed()) {
-            requestedPower = Math.min(requestedPower + 0.01, 1);
+            requestedPower = Math.min(requestedPower + 0.1, 1);
         }
         if (gamepad1.dpadDownWasPressed()) {
-            requestedPower = Math.max(requestedPower - 0.01, -1);
+            requestedPower = Math.max(requestedPower - 0.1, 0);
         }
         if (gamepad1.aWasPressed()) {
             isActive = false;
@@ -48,10 +52,10 @@ public class RobotTuner extends OpMode {
         if (gamepad1.yWasPressed()) {
             isActive = true;
         }
-        if (leftInserter == null || rightInserter == null) {
+        if (leftInserter != null && rightInserter != null) {
             if (gamepad1.xWasPressed()) {
-                leftInserter.setPower(requestedPower);
-                rightInserter.setPower(-requestedPower);
+                leftInserter.setPower(1);
+                rightInserter.setPower(-1);
             }
             if (gamepad1.bWasPressed()) {
                 leftInserter.setPower(0);
@@ -60,14 +64,19 @@ public class RobotTuner extends OpMode {
         }
 
 
-        if (motors[selectedMotor] == null) {
+        if (motors[selectedMotor] != null) {
             if (isActive) {
                 motors[selectedMotor].setPower(requestedPower);
             } else {
                 motors[selectedMotor].setPower(0);
             }
+        } else {
+            telemetry.addLine("Selected motor can not be found");
         }
+        telemetry.addData("Current Motor", selectedMotor);
+        telemetry.addData("Requested power", requestedPower);
         telemetry.addData("Selected motor power", motors[selectedMotor].getPower());
+        telemetry.addData("Selected motor is active", isActive);
         telemetry.update();
     }
 }
