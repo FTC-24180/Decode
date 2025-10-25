6059a59326226ffebd9edb18aefb26f41c757666
Vullarim <swdurda@gmail.com>
2025-10-25 17:34:31 -0500

    Created shell of teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/Decode_Qualifier1_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/Decode_Qualifier1_Teleop.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.BBcode;
 
-public class Decode_Qualifier1_Teleop {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.BBcode.MechanismControllers.Intake;
+import org.firstinspires.ftc.teamcode.BBcode.MechanismControllers.Launcher;
+import org.firstinspires.ftc.teamcode.BBcode.MechanismControllers.Stoppers;
+import org.firstinspires.ftc.teamcode.BBcode.MechanismControllers.Transfer;
+
+@TeleOp (name = "*Main TeleOp")
+public class Decode_Qualifier1_Teleop extends OpMode {
+    Intake intake;
+    Launcher launcher;
+    Stoppers stoppers;
+    Transfer transfer;
+
+    enum State {
+
+    }
+    @Override
+    public void init() {
+        intake = new Intake(this);
+        launcher = new Launcher(this);
+        stoppers = new Stoppers(this);
+        transfer = new Transfer(this);
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
