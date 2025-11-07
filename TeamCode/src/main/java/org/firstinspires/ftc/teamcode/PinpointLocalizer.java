f509abd03f7df902f4e86bb17ddaed83a39d33bc
Allen Durda <60194370+jadurda@users.noreply.github.com>
2025-11-07 16:52:38 -0600

    Removed old Pinpoint driver and updated perpXTicks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public final class PinpointLocalizer implements Localizer {
     public static class Params {
         public double parYTicks = 0; // y position of the parallel encoder (in tick units)
-        public double perpXTicks = -6.624; // x position of the perpendicular encoder (in tick units)
+        public double perpXTicks = -6.5; //6.624; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
